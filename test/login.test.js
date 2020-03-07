7990eb0cdd492e396cea21b660403a5131bd6590
shivaPrabhakarL <prabhakar.lingala@pramati.com>
2020-03-07 23:15:05 +0530

    added  testing

--- test/login.test.js
+++ test/login.test.js
@@ -10,7 +10,7 @@ const html = fs.readFileSync(path.resolve(__dirname,'../views/loginf.ejs'),'utf8
 const indexhtml = fs.readFileSync(path.resolve(__dirname,'../views/index.ejs'),'utf8');
 
 jest.dontMock('fs');
-//var appl;
+
 beforeAll(()=>{
    dbConnect();
 
@@ -30,13 +30,18 @@ beforeAll(()=>{
  test('Login post url',async function(){
    var res = await req(app).post('/login')
    .send({logusername: 'prabha_79',logpassword:'prabha_7'});
-   expect(res.text).toBe("Found. Redirecting to /chat");
- });
+   
+expect(res.text).toBe("Found. Redirecting to /chat");
+});
+
+ 
 
  test('Register get url', async function(){
     var res = await req(app).get('/register');
     expect(res.text).toBe(indexhtml);
  });
+
+
 
  test('Register post url', function(){
   
@@ -48,6 +53,36 @@ beforeAll(()=>{
    
  });
 
- test('Chat get url',function(){
+ 
+//  beforeEach(async function(){
+//       var res = await req(app).post('/login')
+//       .send({logusername: 'prabha_79',logpassword:'prabha_7'});
+//       var token;
+//       loginVerification("prabha_79","prabha_7",function(err,data){
+//          token = data.token;
+//       });
+//       Object.defineProperty(window.document, 'cookie', {
+//          writable: true,
+//          value: 'w_auth='+token,
+//      });
+//       res.send();
+//      // expect(res.text).toBe("Found. Redirecting to /chat");
+//  });
 
- });
+//  test('Chat get url',async function(){
+//     var data1 ;
+    
+//    var res = await req(app).get('/chat');
+//    expect(res.text).toBe(chatHtml);
+ 
+// });
+
+
+// it('should save cookies', function(done) {
+//    loginVerification("prabha_79","prabha_7",function(err,data){
+//       expect(data.name).toBe('shivai7sdfg');
+//    agent
+//    .get('/login')
+//    .expect('set-cookie', 'express.sid=s%3A7JgHgkXGUdI324IyR7vQVFru1y-Ik5MK.g5d2Pn2Thf5bGZFRDN%2BGC93bCGwQhS0adtyXQQdPyy8; Path=/', done);
+//  });
+// });
